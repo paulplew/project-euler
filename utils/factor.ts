62f86b9c099a1dfbf822791e4b508f8dd40c5fc6
Paul <thepaulplew@gmail.com>
2021-11-26 14:24:27 -0500

    Add Trial division factoring algorithm in utils

--- utils/factor.ts
+++ utils/factor.ts
@@ -5,21 +5,12 @@
  * @returns {number[]} an array of the factors of the given number
  */
 export const factor = (n: number): number[] => {
-  /**
-   * A helper for {@link factor} that stores the factors so far.
-   * @private
-   * 
-   * @param  {number} n the number to be factored
-   * @param  {number[]} soFar the factors so far
-   * @returns {number[]} a list of the factors of the remaining n
-   */
-  const _factorHelper = (n: number, soFar: number[]): number[] => {
-    if (n % 2 === 0) {
-      soFar.push(2);
-      return _factorHelper(n / 2, soFar);
-    } 
+  const factors: number[] = [];
 
+  for (let i = 0; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      factors.push(i, n / i);
+    }
   }
-  
-  return [];
+  return factors;
 }
